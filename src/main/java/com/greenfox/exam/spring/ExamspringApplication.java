1afe79c9c56d00fef404d2afe79d33b7ecc520e8
VVarro <virag.varro@gmail.com>
2017-06-06 09:43:30 +0200

    create Spring project

---
+++ src/main/java/com/greenfox/exam/spring/ExamspringApplication.java
@@ -0,0 +1,12 @@
+package com.greenfox.exam.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamspringApplication.class, args);
+	}
+}
